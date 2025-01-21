bae4382f213bd81582b23b877cbdcedd8b78bb69
Ragul Duraisamy <Ragul.Duraisamy@expleogroup.com>
2025-01-21 11:22:24 +0530

    Udpdated runner with TEstng

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -1,11 +1,8 @@
 package TestRunner;
- 
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-@RunWith(Cucumber.class)
 @CucumberOptions(
    
                 features={"src/test/resources/Features"},
@@ -16,7 +13,7 @@ import io.cucumber.junit.CucumberOptions;
                        "html:target\\htmlreport.html"}
  
 )
-public class Runner {
+public class Runner extends AbstractTestNGCucumberTests {
    
    
 }
